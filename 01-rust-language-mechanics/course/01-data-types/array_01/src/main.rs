c0a09e67f9489411aa7d078528012dc0c6ab49a5
Verron <alislehnia@gmail.com>
2026-06-20 02:11:11 +0330

    up

--- 01-rust-language-mechanics/course/01-data-types/array_01/src/main.rs
+++ 01-rust-language-mechanics/course/01-data-types/array_01/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    //packet data
+    let mut packet_data = [100, 200, 300];
+    packet_data[1] = 250;
+    let number_packet = packet_data.len();
+    println!("{number_packet}");
+
+    //buffer
+    let mut _buffer: [u8; 64] = [0; 64];
+
+    //cpu cores
+    let cpu_cores = [2.4, 3.2, 2.8, 3.5];
+    let number_len = cpu_cores.len();
+    let last_cores = cpu_cores[3];
+    println!("{last_cores}");
+    println!("{number_len}");
+    //display cpu_cores
+    println!("{:?}", cpu_cores);
 }
+
