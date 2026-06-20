1d7acb5978554a94078184566605d22cd130b27f
Verron <alislehnia@gmail.com>
2026-06-21 02:40:38 +0330

    up

--- 01-rust-language-mechanics/course/01-data-types/tuple_02/src/main.rs
+++ 01-rust-language-mechanics/course/01-data-types/tuple_02/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let cpu_info: (&str, u8, f64) = ("i5 13400", 10, 4.60);
+    // let frequency = cpu_info.2;
+    // println!("CPU Frequency:{frequency}");
+    // println!("{cpu_info:#?}");
+
+    let (name, cores, frequency) = cpu_info;
+    println!("Model:{name}\ncores:{cores}\nFrequency:{frequency}",);
 }
