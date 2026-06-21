d9d755a165d65fdc18d6b5cb0cb80016c058a70c
Verron <alislehnia@gmail.com>
2026-06-21 16:03:27 +0330

    up

--- 01-rust-language-mechanics/course/01-data-types/tuple_02/src/main.rs
+++ 01-rust-language-mechanics/course/01-data-types/tuple_02/src/main.rs
@@ -6,4 +6,20 @@ fn main() {
 
     let (name, cores, frequency) = cpu_info;
     println!("Model:{name}\ncores:{cores}\nFrequency:{frequency}",);
+
+    //ranges
+    let range = 0..=100;
+    for number in range {
+        println!("{number}");
+    }
+
+    let character = 'a'..='z';
+    for character_display in character {
+        print!("{character_display}");
+    }
+
+    let color = ["Red", "Green", "Blue"];
+    for color_display in color {
+        println!("{color_display}");
+    }
 }
