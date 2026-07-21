77430f58fe52a08272b2a248398de5be9b1205fb
Verron <alislehnia@gmail.com>
2026-07-21 14:24:02 +0330

    up

--- 01-rust-language-mechanics/course/02-functions/parameters_and_arguments/src/main.rs
+++ 01-rust-language-mechanics/course/02-functions/parameters_and_arguments/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     swim_in_profit();
     open_store("Queens");
     bake_pizza(8, "mashroom");
+    close_stor("Harlem", 118, 2124, "11 PM");
 }
 
 fn open_store(neighberhood: &str) {
@@ -16,4 +17,10 @@ fn bake_pizza(number: u8, topping: &str) {
 
 fn swim_in_profit() {
     println!("so much $$$, so little time");
+}
+
+fn close_stor(location: &str, sold_pizzas: u8, revenue: i32, time: &str) {
+    println!(
+        "Closing my pizza store in {location}. We sold {sold_pizzas} pizzas today, made ${revenue}, and closed at {time}."
+    );
 }
