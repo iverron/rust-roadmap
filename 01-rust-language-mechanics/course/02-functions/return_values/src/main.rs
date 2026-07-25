aebd11b2bf29bdcca631842d0534937f6a4bb1bb
Verron <alislehnia@gmail.com>
2026-07-25 13:23:40 +0330

    up

--- 01-rust-language-mechanics/course/02-functions/return_values/src/main.rs
+++ 01-rust-language-mechanics/course/02-functions/return_values/src/main.rs
@@ -1,1 +1,21 @@
-fn main() {}
+fn main() {
+    let result = sqaure(5);
+    println!("The square of 5 is {result}");
+
+    let result = sqaure(24);
+    println!("The square of 24 is {result}");
+
+    let result = sqaure(11);
+    println!("The square of 11 is {result}");
+
+    let result = double_num(888);
+    println!("The double of 888 is {result}");
+}
+
+fn sqaure(number: i32) -> i32 {
+    return number * number;
+}
+
+fn double_num(number: i64) -> i64 {
+    number * 2
+}
